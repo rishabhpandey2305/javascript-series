b68be2b0ce88eb7b47cf375d290c3c68ab75a5ac
hack_2305 <106096818+rishabhpandey2305@users.noreply.github.com>
2024-07-07 17:48:12 +0000

    conversionOperation.js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+let score = "123abc";
+console.log(typeof(score));
+let valueNumber = Number(score);
+console.log(typeof(valueNumber));
+console.log(valueNumber);
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1 ; false => 0
+*/
