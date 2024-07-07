8d44dd3037f7fe01786d5622e8017fa302128f41
hack_2305 <106096818+rishabhpandey2305@users.noreply.github.com>
2024-07-07 16:52:44 +0000

    01_basics_Keywords

---
+++ 01_basics/01_basics.js
@@ -0,0 +1,11 @@
+const id = 1234;
+let name = "rishabh";
+var rollNo = 101;
+rollNo = 102;
+name = "rishabh pandey";
+let number;
+console.table([id,name,rollNo,number]);
+
+/*
+In Javascript we will only use const and let 
+*/
